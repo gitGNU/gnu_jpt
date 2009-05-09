9527c3dd49848d83a208ad0d91674ce1afd251b3
Morten Hustveit <mortehu@m.(none)>
2009-05-09 19:37:32 +0000

    Added PHP stress test.

---
+++ php-stress-test.php
@@ -0,0 +1,248 @@
+<?
+$tokens = array(
+  "SPJUGWKLMMFRPIQ", "CGZRQPGP", "FPK", "JMPBDPPKUJYGSLVIMHPKYFOYPTXJVD",
+  "TRBWCTJFNCBCQ", "WKIBIYHDFTDTKOD", "UOHYYIBDZWXQZLZZKVLLW", "VYCPDP",
+  "BFQTUVNSVBURTRINKZUG", "JVZ", "FVSMRZ", "XQWPPSUQGYDZSVGMLWUPTVMKHLPCU",
+  "BKGPUXNUCSXVROBFZZJDSBVNFJQM", "IDLZRVRHZRP", "KYI", "NVIWXCTXKKIMLL",
+  "CMPDOPVFDWUJOQV", "KQT", "WBMV", "SLXVQKFQ", "JXUJDFUWLNUCDV", "JIMJJTGRF",
+  "ZVJG", "DKZIWKMFUZMJDZPV", "PDMUQ", "DBPRLTWLTVKYTMWOIMXGUMIWFRYPGJHXCHSMDWN",
+  "FDXQVVF", "XYLRTJMTFSVMOKTCYNBZOZMJZN",
+  "LZNWUSWPSLQDOYUUOHQFUQKDJBQYTBSCGVRRNHJUBQ", "VVQPYLKPO", "IXS",
+  "ONIVXNPDWKPYX", "VID", "DJLZFKFGHZ", "UGJLBY", "FQZXQFKTQJOXTDDCQYC",
+  "OLYOKB", "ZJTLBWOMTCVFJD", "FCC",
+  "XUPQUQPVTXCOTPOYDCRKRUQWGGGCMTIRNFHLHLSZFJ",
+  "BCJFLNPYNBNLGPBVODONRSVQMWVBKXNFTJQYUNMNPU", "RDUZQRT", "FHULHD",
+  "OXPHRFLKWTWSKU", "PGR", "PLTHZUVUUVBHIXPCMOMJHIMVWFUN", "YDJ", "HBKQBJMZDWR",
+  "FWGMDQHRMNHXDXRYZYKIPH", "BIBORH", "CITNS", "CJUWMKLMOMFVHQF", "PHCTQS",
+  "MIOTBYHPCIMPIT", "KKKS", "KDMNU", "MPXSBVDUYR", "ILJ", "IXWC",
+  "OPTTDOSWXSYUIYUI", "OXXTZPCBDBYJKP", "KXFYPMJRVTMGYTTQYBLVSCDLZT", "KVQSF",
+  "QYTPS", "WTIVYVPHHMKNB", "HBHKNTLLN", "BTTJNFFWUXRKHYJJPNFLMBSJRSZL",
+  "BBQVHKBNTQLGGOWGGTVT", "RLVQJ", "ODJNCYWVOZXDVZZTDBZ", "PSCJWWBFUOVDZOFU",
+  "TMDRNQ", "RCFVZMGRCRLOY", "CSBRNFYH", "QOQ", "WOYRS", "PFI",
+  "OILJIDRHPKVGCKPCMHKDGT", "ZQRZKYTSNSIUYQSMOHIBCSD", "HNNNWIY",
+  "QXLRNHOGPIOCRWJSVGJQFOGHVYGPIFMQXQVQSUICUDTVKZLRPBYYKSJ", "CIWCMZLH", "OOS",
+  "IRBUVOHBHFFRVDWXLFNNYYXHY", "RIRCUB", "WNOT", "NINGFHZKSNJQOMHJZZSUCFN",
+  "WRUIHXOWMTWTQZUGKR", "GOYRMGXHUTMCUOVTOGMGVWJPCSMQRGWDWXRNGIIY", "DUTQFJF",
+  "OLZZDJPQR", "XTHOW", "DSPBF", "PZOGXUXY", "LBDNWUSXCTNRZKN", "BPOIZKZSHKF",
+  "YOV", "DK", "TUISJKKNJZLYDXIYWWGCJRRHB", "SNC",
+  "RNMXOXUJBWZSSRQIZBPFUDDRTDXOWDRWHOMZTU", "HQW", "WOS",
+  "XXIDBRVPTJYPUKFKVCTQY", "MFXKDBSQMOXZBCBBLQHRCWD", "YCMJDITF", "NZYODG",
+  "FZQLV", "BROZYJXFGTFN", "IIQCTBSTNLTZHRHUMDMNCRQCUJJMOMWNLBSNP", "OYROM",
+  "NLTWPKBBPOFRSLVLOXJ", "BCFWJBKVUVUHGYHKRZTBUBRJIMW",
+  "XBCTGFQSHLRDQHLMGLKUWQMJJKTCOCVWVMKNWXTLI",
+  "GBCKOSIKJVXQOJTVBVJCRGYCXYORKNZHYINRLHBG", "SNQTYBYQ", "FKQURXVHYIUH",
+  "RRQZKYW", "MDX", "TFTLVLC", "PXUYBZUCSULTKOCTKSWUTNDZ", "TQJUDYG", "KFNZD",
+  "HHFYBJCQPHTFRQJFUOVHOUYMV", "TCLILJFRWX", "ZBWZWMJSHMOUXLU",
+  "ZXDIUPFLRVDJNKGQM", "RTFWBVB", "JVLUHQUYIG", "MKTXUWGIQZSGKCSUSRZMIWF",
+  "RGWOUHCYOMZWUWMOXTKFWHOTTGDOMLXMJRHUPRGYHQH", "TVLNPVZTYLRHCJRYIYTIYCMUGBK",
+  "UMPK", "HHLYYN", "QHMBYHJLVXOOSOFG", "PNKRJVZTCSJTQ", "JLDJMRDSKX", "ZQGKX",
+  "DFVOQUMGWPGTMQTZS", "HCWJVFPJHLHBPNWLWVBDRPVYRM", "FQPQCHI", "MC", "HIDBNNDK",
+  "XLNPQ", "RUG", "PUISWH", "LDQIQTTWIPIKHYQ", "CHFJKL",
+  "UIMLIXQJRJVIJMXUDRIICMFJJXNOQLZTNJUU", "SOFZWVMCVJBIXSXTFNLGYV",
+  "FVKUUGJIFMSOJBBRQHJJHHHHYQCWRNHG", "DQXSKHIXJZVUPSQF", "DRPIZHYIHMTUIZDK",
+  "GSFMNJPYTWITNYNLJKLMN", "OINSIQVBXKH",
+  "VQZXFKNTDVQUPWOBXPFNIZPFJBOHGSDPUDDVQPPFBTZM",
+  "GBIYLNYHSUQFQBICMVBCWPMDRTRQMCOXJSXBMPIHQQZMQFWXJKBUMGLZZK",
+  "STKDZSRPTXXOUIMIVSHUQOCKQX", "GUUFRNVL", "VPRNPDVU", "OBULMJZUNMRWD",
+  "BDYPCUYUSGLSUYK", "ZVHSHRDO", "JRQSHPUOIB", "NFGXOSIUPVKBZLZUM",
+  "NLVTKUVWMTSUKIQDIUNF", "BIFVMOMMSIDZBXIPRMMJNNPOHVRNRMLL", "KVQKMTQGLKQNIOJR",
+  "BLVSJCOUOPVDBIDZHOBSQVCMZGVYWMTVSXSNX", "GBQRTL", "IUUGT", "QV",
+  "RITTMCUBXZBHVLJIDRDFGHHD", "ULPVXN", "CXYTJOJRTKLNIK",
+  "TDCGTVCPKKBGKQYDBIPZTLORJUZFSSOOYRYBSD", "DPLCFMHUTWNR",
+  "JGBGHXPTHNGZSGSKFSJMNBZIYUTHDVTLNNOGMGLBBH", "ISS",
+  "SGSNNIZFVUBQKPXKLCPHHOZRTIH", "JOHT", "XYTGSCJTIWCD", "TLKSOCOWGFIVZWL",
+  "RTWBLJOCVDGUCDJFKQV", "FMIGYUVXVXNPTWZQNZS", "UKNQIBOWXIVCIYU",
+  "RLZXTYQXYKFPIVFXQXJVRYYSBFSYIGMVDCIZTQXRCXSDJMSWUCBXNPBOBDYIDODOUDYCVJPHHUQHWMQPRITD",
+  "GJGLYVO", "VXT", "WWMJPPLDRP", "UZQXQFCFXHNPOI", "KMSBGSHKTCU",
+  "TQVGHHHYOQIVNQVOPDDWJPHNNZOVDBKPH", "GPCCQBXRLVGSJJLLOXXHPBMNXJ",
+  "SQTJLRJDZWWRTNRXNQQ", "HLSYDLGKQPFBUHONV", "VPOOMJ", "YVPDMZXKS",
+  "XTINKJIQLIBBCVLVJXNKYRUVSVKFTUKYU", "ZRNCYUFV", "JMVLLVNCTMFXRTZKKXFQ",
+  "DFJGVHW", "LWJOIF", "KXCHLBWVKQWUKYXYJQIFXP", "MJ", "MVWBIRRJNTGJHWJTKUUKRCP",
+  "YUUOGSRQRJCOW", "QUT", "FRILSRWWKQKLL", "VKTOU", "KWSN", "CHGVUVKWBUXRWDMJH",
+  "QRMQQJTLGYDZFZXIBGDV", "MPLSUHMFTSNTWRQFLJYK", "SUPWNMIFGKFSJHHF",
+  "IXSSIFPWSSLSRZL", "BLN", "KLJKMJGTJ", "XKLPZV",
+  "TGWYUIFMZQVWILQTZRFLRNWRUVQOJVDYINZSOTDHCIHZTXWSFQSFNYLMVGPJGH", "NODNLUM",
+  "JTRLTWPQLYB", "MCYPHG", "JJNCZR", "UZLGOYV",
+  "OHXZRDUBNGPFYMXPJFFBLKRWJZMKBSUVBWQJTTVTZUQYTLMLNUCBJPVSWUHMLK",
+  "HRPLJVOQVDBJLJJMW", "NTGB", "RVS", "WDBFYRKVUKRJP", "PNIVXPGHQF",
+  "GMXUGVIMWZQWCDNQGDUOBTO", "DICL", "HKVJCL", "SDPDNSWDYXNDDZZTR", "VXRGL",
+  "MOWL", "ZXOXUKU", "QRTNKKZ", "NUMSXVRZ", "VSIKRLYBVLTIBRIIHBI", "UWL", "FBQW",
+  "HYFG", "LDKNPSOYTJRVOWZNYZOV", "ZZVQLW", "DXJJGMD", "ZOMKJZJGQXZTUY",
+  "OXFJIZIV", "KF", "ZQDIKDQNKHWNQU", "WYKMWNF", "WLXZZXJPIHKFVGPOKH",
+  "JCOWXNSK", "UYIPZURKYQZBKTYUGPTFSQOQW");
+
+
+$test_count = 0;
+
+$values = array();
+
+$token_count = sizeof($tokens);
+
+unlink("/tmp/test-db.tab");
+unlink("/tmp/test-db.tab.log");
+
+$db = djpt_init("/tmp/test-db.tab");
+
+if($db === false)
+{
+  echo("djpt_open failed: " . djpt_last_error() . "\n");
+
+  exit;
+}
+
+$test = 0;
+srand(0);
+
+$rev_key = array();
+
+for($i = 0; $i < sizeof($tokens); ++$i)
+  $rev_key[$tokens[$i]] = $i;
+
+for(;;)
+{
+  $row = rand(0, $token_count - 1);
+  $col = rand(0, $token_count - 1);
+  $token = rand(0, $token_count - 1);
+  ++$tests;
+
+  $v = &$values[$row][$col];
+
+  echo "\r$tests tests. ";
+
+  switch(rand(0, 6))
+  {
+  case 0:
+
+    {
+      if(false === djpt_replace($db, $tokens[$row], $tokens[$col], $tokens[$token]))
+      {
+        echo "djpt_replace failed: " . djpt_last_error() . ".\n";
+
+        exit;
+      }
+
+      if($tokens[$token] !== djpt_get($db, $tokens[$row], $tokens[$col]))
+      {
+        echo "djpt_get after djpt_replace failed: " . djpt_last_error() . ".\n";
+
+        exit;
+      }
+
+      $v = $tokens[$token];
+    }
+
+    break;
+
+  case 1:
+
+    {
+      if(isset($values[$row][$col]))
+      {
+        if($v !== djpt_get($db, $tokens[$row], $tokens[$col]))
+        {
+          echo "djpt_get failed to get value: " . djpt_last_error() . "\n";
+
+          exit;
+        }
+      }
+      else
+      {
+        if(false !== djpt_get($db, $tokens[$row], $tokens[$col]))
+        {
+          echo "djpt_get succeeded unexpectedly.\n";
+
+          exit;
+        }
+      }
+    }
+
+    break;
+
+  case 2:
+
+    {
+      if(isset($values[$row][$col]))
+      {
+        if(false === djpt_remove($db, $tokens[$row], $tokens[$col]))
+        {
+          echo "djpt_remove failed: " . djpt_last_error() . ".\n";
+
+          exit;
+        }
+      }
+      else
+      {
+        if(false !== djpt_remove($db, $tokens[$row], $tokens[$col]))
+        {
+          echo "djpt_remove succeeded unexpectedly.\n";
+
+          exit;
+        }
+      }
+
+      unset($values[$row][$col]);
+    }
+
+    break;
+
+  case 3:
+
+    {
+      $colname = $tokens[$col];
+      $data = djpt_column_scan($db, $colname, 0);
+
+      foreach($data as $key => $value)
+      {
+        if($value != $values[$rev_key[$key]][$col])
+        {
+          echo "Unexpected scan result, at $key/$colname: $value != " . $values[$rev_key[$key]][$col] . "\n";
+
+          exit;
+        }
+      }
+    }
+
+    break;
+
+  case 4:
+
+    {
+      if(false === djpt_append($db, $tokens[$row], $tokens[$col], ""))
+      {
+        echo "Failed to append nothing: " . djpt_last_error() . ".\n";
+
+        exit;
+      }
+
+      $v .= "";
+    }
+
+    break;
+
+  case 5:
+
+    {
+      if(false !== djpt_append($db, "", $tokens[$col], ""))
+      {
+        echo "Succeeded in writing to zero length row name.";
+
+        exit;
+      }
+    }
+
+    break;
+
+  case 6:
+
+    {
+      if(false !== djpt_append($db, $tokens[$row], "", ""))
+      {
+        echo "Succeeded in writing to zero length column name.";
+
+        exit;
+      }
+    }
+
+    break;
+  }
+}
